c9db0a76590862e11a19e67d95eee33d998a5365
Maya Legris <maya.legris@gmail.com>
2023-05-09 11:40:33 +0200

    Ajout de la navbar

--- ExpoProject/parametres.js
+++ ExpoProject/parametres.js
@@ -1,26 +1,21 @@
 import * as React from 'react';
-import MapView from 'react-native-maps';
 import { StyleSheet, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function Map() {
+export default function Parametres() {
     return (
-      <View style={styles.container}>
-        <MapView style={styles.map} />
-      </View>
+        <View style={styles.container}>
+            <Text>Paramètres !</Text>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backroundcolor: '#fff',
-      alignItems: 'center',
-    },
-    map: {
-      width: '100%',
-      height: '100%',
-    },
-  });
+  container: {
+    flex: 1,
+    backroundcolor: '#fff',
+    alignItems: 'center',
+  },
+});
