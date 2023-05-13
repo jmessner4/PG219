e0fbd46e636f56c02b7785e08f17d614bdc3ebf2
CHEBBI <achebbi@bordeaux-inp.fr>
2023-05-13 16:03:36 +0200

    opérations sur les caches

--- client/screens/homescreen.js
+++ client/screens/homescreen.js
@@ -1,35 +1,35 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-import Parametres from './parametres';
-import Map from './map';
-import Balises from './balises';
+import Parametres from "./parametres";
+import Map from "./map";
+import Balises from "./balises";
 
 const tab = createBottomTabNavigator();
 
 export default function Homescreen() {
-    return(
-        <NavigationContainer independent={true}>
-            <tab.Navigator screenOptions={ ({route}) => ({
-                tabBarIcon: ({focused,color,size}) => {
-                let iconName;
-                if (route.name == 'Carte') {
-                    iconName = "map-outline"
-                } else if (route.name == 'Paramètres') {
-                    iconName = "settings-outline"
-                }
-                else if (route.name == 'Balises') {
-                    iconName = "location-outline"
-                }
-                return <Ionicons name={iconName} size={25} />
-                }
-            })}>
-                <tab.Screen name='Balises' component={Balises} />
-                <tab.Screen name='Carte' component={Map} />
-                <tab.Screen name='Paramètres' component={Parametres} />
-            </tab.Navigator>
-        </NavigationContainer>
-
-    )
+  return (
+    <NavigationContainer independent={true}>
+      <tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name == "Carte") {
+              iconName = "map-outline";
+            } else if (route.name == "Paramètres") {
+              iconName = "settings-outline";
+            } else if (route.name == "Balises") {
+              iconName = "location-outline";
+            }
+            return <Ionicons name={iconName} size={25} />;
+          },
+        })}
+      >
+        <tab.Screen name="Balises" component={Balises} />
+        <tab.Screen name="Carte" component={Map} />
+        <tab.Screen name="Paramètres" component={Parametres} />
+      </tab.Navigator>
+    </NavigationContainer>
+  );
 }
--- server.js
+++ server.js
@@ -14,14 +14,33 @@ app.use(bodyParser.json())
 mongoose.connect(url_db)
 .then(()=>console.log("connected to db"))
 .catch(err=>console.log("error"))
-
+//Création d'une fonction qui controle si la valeur d'un champ est vide
+function vide(valeur){
+    if ((valeur === null)||(typeof (valeur) === "string" && valeur.trim().length === 0) ||
+    (typeof(valeur)=== "number" && valeur === 0)){
+return true;
+    }
+else{
+    return false;
+}
+}
 // en cas de requete create
-app.post('/create',(req, res)=>{
-  //on peut aussi utiliser la fonction create qui nous retourne l'element crée
-   caches_model.collection.insertOne(req.body)
-   .then(cache=>res.status(200).json(cache))
-   .catch(res.status(400))       
-    })  
+app.post('/create',async(req, res)=>{
+//le serveur vérifie si le champ id est unique et  non vide
+  if(vide(req.body.id)||vide(req.body.difficulte)
+  ||vide(req.body.latitude)||vide(req.body.longitude)){
+    return res.status(404).json({message: "vide"});
+  } 
+//si ce n'est pas vide on vérifie si l'id existe ou pas 
+else{const cache = await caches_model.collection.findOne({id: req.body.id});
+if(cache)
+{return res.status(404).json({message: "cache existante"})}
+else{
+//on peut aussi utiliser la fonction create qui nous retourne l'element crée
+caches_model.collection.insertOne(req.body)
+.then(()=>{return res.status(200).json({message: "success"})})
+.catch(res.status(400))
+} }})
 //en cas de requete de récupération
 app.get('/caches',(req, res)=>{
      caches_model.find({})
@@ -29,11 +48,15 @@ app.get('/caches',(req, res)=>{
     .catch(res.status(400))       
      })  
 //update
-app.put('/update/:id',(req, res)=>{
+app.put('/update/:id',async(req, res)=>{
+    if(vide(req.body.difficulte)
+    ||vide(req.body.latitude)||vide(req.body.longitude)||vide(req.body.description)){
+      return res.status(404).json({message: "vide"});
+    } else{
     caches_model.updateOne({id : req.params.id},req.body)
-   .then(caches=>res.status(200).json(caches))
-   .catch(res.status('400'))       
-    })  
+   .then(()=>{return res.status(200).json({message: "success"})})
+   .catch(res.status(400))       
+    }}) 
 //delete
 app.delete('/delete/:id',(req, res)=>{
     caches_model.deleteOne({id : req.params.id})
